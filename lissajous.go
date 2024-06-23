2087cd219113d91c56f748260fcbaee1b264e1b9
john davis <davis@doom.lan>
2024-06-23 11:11:49 -0400

    wip

--- lissajous.go
+++ lissajous.go
@@ -53,7 +53,8 @@ func main() {
 
 // public entrypoint/function
 func Lissajous(out io.Writer) {
-	lissajous(out)
+//	lissajous(out)
+    fmt.Println("yo") // as a test of $ go clean -modcache
 }
 
 // private entrypoint/function
